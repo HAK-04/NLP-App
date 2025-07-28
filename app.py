1d6a6d951d9af6824d6bb8d6921ac10e1563ec3a
HAK-04 <114519326+HAK-04@users.noreply.github.com>
2025-07-28 08:50:31 +0500

    Update app.py

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ except OSError:
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 logging.info(f"Using device: {device} for BART model.")
 
-model_name = "facebook/bart-large-cnn" # ssleifer/distilbart-cnn-12-6 for moar speed.
+model_name = "sshleifer/distilbart-cnn-12-6" # facebook/bart-large-cnn alt
 
 HF_TOKEN = os.environ.get("HF_TOKEN", None) #IMPORTANT
 
